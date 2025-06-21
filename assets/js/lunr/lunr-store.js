3ea6c3f4b8b2e20971d3e3be76d0f80da76462a1
itsuki <175463719+Itsuki0222@users.noreply.github.com>
2025-06-21 13:43:33 +0900

    Add files via upload

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "サイトがリニューアルしました",
+        "excerpt":"Krypton Linuxのサイトをリニューアルしました。   TNTSuperMan様の提案で、Jekyllを使って構築されています。   かなり前から作っていたのですが、サボりすぎて制作に何ヶ月もかかってしまいました((   とにかく無事に完成したのでこれからはこのサイトでやっていこうと思います。  ","categories": [],
+        "tags": ["informations"],
+        "url": "/2025-06-21/site-renewal/",
+        "teaser": null
+      }]
